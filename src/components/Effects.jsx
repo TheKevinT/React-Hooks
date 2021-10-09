3956ea3fc777dc8574b64476e7880e3aa7a69712
Kevin Tipantiza <kevinftipantiza@gmail.com>
2021-10-08 21:19:32 -0500

    useEffect con return (cleanUp) para evitar ciclos

--- src/components/Effects.jsx
+++ src/components/Effects.jsx
@@ -2,34 +2,51 @@ import React, { useEffect, useState } from "react";
 
 const Effects = () => {
   //crear un state para traer usuarios
-  const [users, setUsers] = useState([]);
+  //   const [users, setUsers] = useState([]);
   //crear un state para id de usuarios
-  const [id, setId] = useState(10);
+  //   const [id, setId] = useState(10);
 
   //traer la data una sola vez
-  useEffect(() => {
-    //peticion a JSON PLACEHOLDER
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then(
-        //asignamos la data a users
-        (data) => {
-          setUsers(data);
-        }
-      );
-  }, []);
+  //   useEffect(() => {
+  //peticion a JSON PLACEHOLDER
+  //     fetch("https://jsonplaceholder.typicode.com/users")
+  //       .then((res) => res.json())
+  //       .then(
+  //         //asignamos la data a users
+  //         (data) => {
+  //           setUsers(data);
+  //         }
+  //       );
+  //   }, []);
 
   //crear otro efecto para hacer otra petición diferente
   //la dependencia que va estar escuchando es el Id
   //se ejecuta cuando el ID cambie
-  useEffect(() => {
-      setUsers([{
-          hola:'cambio en ID'
-      }])
-      console.log(users)
-  }, [id]);
+  //   useEffect(() => {
+  //       setUsers([{
+  //           hola:'cambio en ID'
+  //       }])
+  //       console.log(users)
+  //   }, [id]);
 
-//   console.log(users);
+  //   console.log(users);
+
+  // ---------------------------
+
+  const [state, setstate] = useState(0);
+  const evento =  (e) => {
+    console.log("moviendo");
+  }
+
+ useEffect(() => {
+     if(state === "123"){
+         window.addEventListener("mousemove",evento);
+     }
+     //seccion de limpieza para que no haga ciclos
+     return () => {
+        window.removeEventListener("mousemove",evento);
+     }
+ }, [state])
 
   const _handleSubmit = (e) => {
     e.preventDefault();
@@ -45,8 +62,10 @@ const Effects = () => {
           </label>
           <input
             type="text"
-            value={id}
-            onChange={(e) =>setId(e.target.value)}
+            onChange={e => setstate(e.target.value)}
+            value={state}
+            // value={id}
+            // onChange={(e) => setId(e.target.value)}
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
