a087eb274d0ac3921b214a1c18309388ae72a0ee
Kevin Tipantiza <kevinftipantiza@gmail.com>
2021-10-08 19:25:15 -0500

    UseEffect ejecuta algo una sola vez

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -1,20 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const State = () => {
-
+  // useEffect no retorna nada
+  // maneja el ciclo de vida
+  // se utiliza para que algo se ejecute una sola vez
+  // const [state, setState] = useEffect();
   const [state, setState] = useState(0);
 
-  // setInterval(() => {
-  //   setState(state +1)
-  // }, 2000);
+  useEffect(() => {
+    console.log(state);
+  },[]);
 
-
+  const _handleClick = () => {
+    setState(state + 1);
+  };
 
   return (
     <div className="container text-center">
-      <h1>UseState</h1>
+      <h1>UseEffect</h1>
       <hr />
       cuenta: {state}
+      <button onClick={_handleClick}> +1</button>
     </div>
   );
 };
